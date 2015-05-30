3a2900d169472f6883f9886c584e4d4bf67fc7b4
svarnyjunak <svarnyjunak@gmail.com>
2015-05-30 23:11:06 +0200

    Initial commit

---
+++ allmusic2deezer.js
@@ -0,0 +1,142 @@
+// ==UserScript==
+// @name        Allmusic @ Deezer
+// @namespace   svarnyjunak
+// @include     http*://www.allmusic.com/*
+// @version     1
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+"use strict";
+(function () {
+    try {
+        var deezer = {
+            makeRequest: function(url, callback) {
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: url,
+                    onload: function(response) {
+                        callback(JSON.parse(response.responseText));
+                    }
+                })
+            },
+            getArtist(artistName, callback) {
+                var url = 'https://api.deezer.com/search/artist/?q=' + artistName + '&index=0&limit=5&output=json';
+                this.makeRequest(url, callback);
+            },
+            getAlbum(albumName, callback) {
+                var url = 'https://api.deezer.com/search/album/?q=' + albumName + '&index=0&limit=5&output=json';
+                this.makeRequest(url, callback);
+            }
+        };
+
+
+        var editorsChoice = {
+            getArtistName: function(choice) {
+                var artistElement = choice.getElementsByClassName('artist')[0];
+                if(artistElement === undefined) {
+                    return undefined;
+                }
+
+                var anchor = artistElement.getElementsByTagName('a')[0];
+                if(anchor === undefined) {
+                    return undefined;
+                }
+                return anchor.textContent;
+            },
+            getAlbumName: function(choice) {
+                var titleElement = choice.getElementsByClassName('title')[0];
+                if(titleElement === undefined) {
+                    return undefined;
+                }
+
+                var anchor = titleElement.getElementsByTagName('a')[0];
+                if(anchor === undefined) {
+                    return undefined;
+                }
+                return anchor.textContent;
+            }
+        };
+
+        var LinkAdder = function() {
+            this.artistsJson = undefined;
+            this.albumsJson = undefined;
+            this.deezerClassName = 'deezer-link';
+            this.getDeezerLinkUrl = function() {
+                var artists = this.artistsJson.data;
+                if(artists.length === 0) {
+                    return;
+                }
+
+                var albums = this.albumsJson.data;
+                var artist = artists[0];
+                for(var i = 0;i<albums.length;i++) {
+                    var album = albums[i];
+                    if(album.artist.id === artist.id) {
+                        return album.link;
+                    }
+                }
+            };
+        };
+
+        LinkAdder.prototype = {
+            linkAlreadyAdded: function(element) {
+                return element.getElementsByClassName(this.deezerClassName).length > 0;
+            },
+
+            tryAddLinkToDeezer: function(element) {
+                if(this.artistsJson === undefined || this.albumsJson === undefined) {
+                    return;
+                }
+
+                var link = document.createElement("a");
+                link.style.fontSize = 'larger';
+                link.classList.add(this.deezerClassName);
+
+                var url = this.getDeezerLinkUrl();
+                if(url !== undefined) {
+                    link.href = url;
+                    link.textContent = 'Play on deezer!';
+                }
+                else {
+                    link.textContent = 'Upps, no album like that found!';
+                    link.style.color = 'Red';
+                }
+
+                if(!this.linkAlreadyAdded(element)) {
+                    var albumCover = element.getElementsByClassName('album-cover')[0];
+                    element.insertBefore(link,albumCover);
+                }
+            }
+        };
+
+
+        var editorsChoices = document.getElementsByClassName('editors-choice');
+        var temp = document.getElementsByClassName('editors-choice');
+        for(var i = 0; i<editorsChoices.length;i++){
+            var choice = editorsChoices[i];
+
+            choice.onmouseover=function(){
+                var currentChoice = this;
+                var linkAdder = new LinkAdder();
+
+                if(!linkAdder.linkAlreadyAdded(currentChoice)) {//ještě jsem ten link nepřidali
+                    var albumName = editorsChoice.getAlbumName(currentChoice);
+                    var artistName = editorsChoice.getArtistName(currentChoice);
+
+                    deezer.getAlbum(albumName, function(json) {
+                        linkAdder.albumsJson = json;
+                        linkAdder.tryAddLinkToDeezer(currentChoice);
+                    });
+
+                    deezer.getArtist(artistName, function(json) {
+                        linkAdder.artistsJson = json;
+                        linkAdder.tryAddLinkToDeezer(currentChoice);
+                    });
+                }
+            };
+        }
+    }
+    catch(e){
+        console.log(e);
+        throw e;
+    }
+})();
